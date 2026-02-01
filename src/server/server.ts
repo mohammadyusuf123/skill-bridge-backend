38685cd063ea75e9851a94be639b442b5d0b77c4
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 01:14:45 +0600

    adding vercel.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,6 @@
 import serverless from "serverless-http";
 import app from "./app"; // Adjust path if needed
 
-export const GET = serverless(app);
-export const POST = serverless(app);
-export const PUT = serverless(app);
-export const PATCH = serverless(app);
-export const DELETE = serverless(app);
+const handler = serverless(app);
+
+export default handler;
