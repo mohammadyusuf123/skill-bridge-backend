9484c15d874c251f41e7642b412751dd2db5f20e
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 00:54:59 +0600

    fix diployment isue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client'
-const connectionString = `${process.env.DATABASE_URL}`
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const connectionString = process.env.DATABASE_URL!;
 
-export { prisma }
+const adapter = new PrismaPg({ connectionString });
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma ?? new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
