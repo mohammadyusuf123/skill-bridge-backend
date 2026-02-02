0621ddb1e27c71405dadcc5f2935bc8bb9be016a
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 13:01:12 +0600

    retive

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
+
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
 
@@ -9,38 +10,25 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
 
-  trustedOrigins: [
-    process.env.APP_URL!, // https://skill-bridge-frontend.vercel.app
-  ],
-
-  cookies: {
-    session: {
-      name: "sb_session",
-      options: {
-        httpOnly: true,
-        secure: true,        // REQUIRED on Vercel
-        sameSite: "none",    // 🔥 THIS WAS MISSING
-        path: "/",
-      },
-    },
-  },
-
-  // You can KEEP this, but it is NOT enough alone
-  advanced: {
-    useSecureCookies: true,
-  },
+  trustedOrigins: [process.env.APP_URL!],
 
   user: {
     additionalFields: {
       phone: { type: "string", required: false },
-      role: { type: "string", required: true },
-      status: { type: "string", required: false },
+       role: {
+        type: "string",
+        required: true
+      },
+       status: {
+        type: "string",
+        required: false
+      }
     },
   },
 
   emailAndPassword: {
-    enabled: true,
-    autoSignIn: true,
-    requireEmailVerification: false,
-  },
+  enabled: true,
+  autoSignIn: true,            // ✅
+  requireEmailVerification: false, // (for dev only)
+}
 });
