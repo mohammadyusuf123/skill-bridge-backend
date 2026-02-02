50a6c6264c75958da15645bffeb151abc949f45d
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 10:38:10 +0600

    adding localhost for test

--- src/server/app.ts
+++ src/server/app.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.APP_URL,
+  origin: process.env.APP_URL || "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 }));
