e0473ff3a8895949714f8315e516a315ae8fe58b
Mohammad Yusuf <yusue786@gmail.com>
2026-02-04 13:26:52 +0600

    adding port

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,11 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+if (!PORT) {
+  throw new Error("PORT environment variable is not set");
+}
+
+app.listen(Number(PORT), () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
 
